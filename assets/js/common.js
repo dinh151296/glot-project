31b436d698326fb386faf0732f5d27cf6f7ec608
ThanhDinh <mr.thanhdinh1996@gmail.com>
2022-02-09 16:29:14 +0700

    new cm

---
+++ assets/js/common.js
@@ -0,0 +1,8 @@
+// menu
+let menuIconToggle = document.querySelector('.burger');
+let menuMain = document.querySelector('.wrap-header__menu');
+menuIconToggle.addEventListener('click', function() {
+  menuIconToggle.classList.toggle('active');
+  menuMain.classList.toggle('show');
+
+})
